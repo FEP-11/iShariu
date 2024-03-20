bf1a8bd7ba1c45bca8b5cbc119fa99d1806f1c70
Dmytro <dsumcow@gmail.com>
2024-03-20 18:48:57 +0200

    Add Sign in page

---
+++ WebApp/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ public class HomeController : Controller
     {
         return View();
     }
-
+     
     public IActionResult Privacy()
     {
         return View();
