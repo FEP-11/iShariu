a397bcf9ae0573128ae9820b15a4f38807bbaa00
pinkstoney <hrynivinstitutionalized@gmail.com>
2024-05-09 19:35:48 +0300

    refactor account and about controllers

--- WebApp/Controllers/AboutController.cs
+++ WebApp/Controllers/AboutController.cs
@@ -3,36 +3,34 @@ using Microsoft.AspNetCore.Mvc;
 using WebApp.Models;
 using WebApp.Services;
 
-namespace WebApp.Controllers;
-
-public class AboutController : Controller
+namespace WebApp.Controllers
 {
-    private readonly ILogger<AboutController> _logger;
-    private readonly MongoDBService<User> _userService;
-    
-    public AboutController(ILogger<AboutController> logger, MongoDBService<User> userService)
+    public class AboutController : Controller
     {
-        _logger = logger;
-        _userService = userService;
-    }
-    
-    [Route("/about/terms")]
-    public async Task<IActionResult> Terms()
-    {
-        var user = await _userService.GetAsync(User.FindFirstValue(ClaimTypes.NameIdentifier));
-        return View(user);
-    }
-    
-    public async Task<IActionResult> Index()
-    {
-        var user = await _userService.GetAsync(User.FindFirstValue(ClaimTypes.NameIdentifier));
-        return View(user);
-    }
-    
-    [Route("/about/privacy")]
-    public async Task<IActionResult> Privacy()
-    {
-        var user = await _userService.GetAsync(User.FindFirstValue(ClaimTypes.NameIdentifier));
-        return View(user);
+        private readonly MongoDBService<User> _userService;
+
+        public AboutController( MongoDBService<User> userService)
+        {
+            _userService = userService;
+        }
+
+        private async Task<User> GetCurrentUserAsync()
+        {
+            return await _userService.GetAsync(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        }
+
+        private async Task<IActionResult> RenderViewAsync(string viewName)
+        {
+            var user = await GetCurrentUserAsync();
+            return View(viewName, user);
+        }
+
+        public Task<IActionResult> Index() => RenderViewAsync(nameof(Index));
+        
+        [Route("/about/terms")]
+        public Task<IActionResult> Terms() => RenderViewAsync(nameof(Terms));
+
+        [Route("/about/privacy")]
+        public Task<IActionResult> Privacy() => RenderViewAsync(nameof(Privacy));
     }
 }
--- WebApp/Controllers/AccountController.cs
+++ WebApp/Controllers/AccountController.cs
@@ -5,50 +5,59 @@ using WebApp.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using MongoDB.Bson.IO;
-using MongoDB.Driver;
 using WebApp.Services;
 
 namespace WebApp.Controllers
 {
     public class AccountController : Controller
     {
-        private readonly ILogger<AccountController> _logger;
         private readonly MongoDBService<User> _userService;
         private readonly MongoDBService<Course> _courseService;
+        private readonly IConfiguration _configuration;
 
-        public AccountController(ILogger<AccountController> logger, MongoDBService<User> settings, MongoDBService<Course> courseService)
+        public AccountController( MongoDBService<User> settings, MongoDBService<Course> courseService, IConfiguration configuration)
         {
-            _logger = logger;
             _userService = settings;
             _courseService = courseService;
+            _configuration = configuration;
         }
-
+        
         [AllowAnonymous]
-        [HttpGet]
-        public ActionResult SignIn() => View();
-
+        [HttpGet("/account/signin")]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        
         [AllowAnonymous]
         [HttpPost("/account/signin")]
         public async Task<ActionResult> SignInAsync([FromForm] User user)
         {
+            User? foundedUser = await GetUserIfExists(user.Username, user.Password);
+
+            if (foundedUser == null)
+                return BadRequest("Invalid username or password.");
+
+            await SignInUser(foundedUser);
+            return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).Replace("Controller", ""));
+        }
+
+        private async Task<User?> GetUserIfExists(string username, string password)
+        {
             List<User> users = await _userService.GetAsync();
-            User? foundedUser = users.FirstOrDefault(u => u.Username == user.Username && u.Password == user.Password);
+            return users.FirstOrDefault(u => u.Username == username && u.Password == password);
+        }
 
-            if (foundedUser == null) return BadRequest();
-
+        private async Task SignInUser(User user)
+        {
             var claims = new List<Claim>()
             {
-                new Claim(ClaimTypes.NameIdentifier, foundedUser.Id),
-                new Claim(ClaimTypes.Role, foundedUser.Role)
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Role, user.Role)
             };
-
-            ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, "Cookies");
-
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            
             await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
-            _logger.LogInformation("Authorization has been successful");
-
-            return RedirectToAction("Index", "Home");
         }
 
         [AllowAnonymous]
@@ -59,7 +68,6 @@ namespace WebApp.Controllers
         public async Task<ActionResult> LogOutAsync()
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-
             return RedirectToAction("Index", "Home");
         }
 
@@ -67,27 +75,15 @@ namespace WebApp.Controllers
         [HttpGet("user/{id}")]
         public async Task<IActionResult> UserProfile(string id = null)
         {
-            if (id == null) id = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            
+            if (id == null)
+                id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
             var user = await _userService.GetAsync(id);
-            if (user == null) return Ok(user);
+            if (user == null)
+                return NotFound();
 
-            var createdCourses = new List<Course>();
-            foreach (var courseId in user.CreatedCourses)
-            {
-                var course = await _courseService.GetAsync(courseId);
-                if (course != null) createdCourses.Add(course);
-            }
-
-            var enrolledCourses = new List<Course>();
-            foreach (var courseId in user.EnrolledCourses)
-            {
-                var course = await _courseService.GetAsync(courseId);
-                if (course != null)
-                {
-                    enrolledCourses.Add(course);
-                }
-            }
+            var createdCourses = await GetCreatedCourses(user);
+            var enrolledCourses = await GetEnrolledCourses(user);
 
             var model = new UserProfileViewModel
             {
@@ -98,63 +94,82 @@ namespace WebApp.Controllers
 
             return View(model);
         }
-        
+
+        private async Task<List<Course>> GetCreatedCourses(User user)
+        {
+            var createdCourses = new List<Course>();
+            foreach (var courseId in user.CreatedCourses)
+            {
+                var course = await _courseService.GetAsync(courseId);
+                if (course != null) 
+                    createdCourses.Add(course);
+            }
+            return createdCourses;
+        }
+
+        private async Task<List<Course>> GetEnrolledCourses(User user)
+        {
+            var enrolledCourses = new List<Course>();
+            foreach (var courseId in user.EnrolledCourses)
+            {
+                var course = await _courseService.GetAsync(courseId);
+                if (course != null)
+                    enrolledCourses.Add(course);
+            }
+            return enrolledCourses;
+        }
+
         [Authorize]
         [HttpPost("user/{id}")]
         public async Task<IActionResult> UpdateProfile(User model, string id)
         {
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             if (userId != id)
-            {
                 return Forbid();
-            }
 
             var user = await _userService.GetAsync(id);
-            if (user == null) return NotFound();
+            if (user == null)
+                return NotFound();
 
-            if (model.Email != null)
-            {
-                user.Email = model.Email;
-            }
-
-            if (model.Username != null)
-            {
-                user.Username = model.Username;
-            }
-
-            if (!string.IsNullOrEmpty(model.Password))
-            {
-                user.Password = model.Password;
-            }
-
-            user.AllowAccessToAgeRestrictedContent = model.AllowAccessToAgeRestrictedContent;
-            user.UseDataToImproveIShariu = model.UseDataToImproveIShariu;
-            
-            if (model.ProfileColor != null)
-            {
-                user.ProfileColor = model.ProfileColor;
-            }
-            
-            user.Location = model.Location;
+            UpdateUserProperties(user, model);
 
             await _userService.PutAsync(user);
-
             return RedirectToAction("UserProfile", new { id = model.Id });
         }
-        
+
+        private void UpdateUserProperties(User user, User model)
+        {
+            var properties = typeof(User).GetProperties();
+            foreach (var property in properties)
+            {
+                var value = property.GetValue(model);
+                if (value != null && property.CanWrite)
+                    property.SetValue(user, value);
+            }
+        }
+
         [Authorize]
         [HttpGet("user/{id}/settings")]
         public async Task<IActionResult> UserSettings(string id)
         {
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            if (userId != id) return Forbid();
+            if (userId != id)
+                return Forbid();
 
             var user = await _userService.GetAsync(id);
-            if (user == null) return NotFound();
+            if (user == null) 
+                return NotFound();
 
-            // Fetch the list of countries
+            var countries = await GetCountries();
+            ViewData["Countries"] = countries;
+
+            return View(user);
+        }
+
+        private async Task<List<Country>?> GetCountries()
+        {
             using var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync("https://restcountries.com/v3.1/all");
+            var response = await httpClient.GetAsync(_configuration["RestCountriesAPI"]);
             var responseString = await response.Content.ReadAsStringAsync();
 
             var options = new JsonSerializerOptions
@@ -162,14 +177,9 @@ namespace WebApp.Controllers
                 PropertyNameCaseInsensitive = true,
             };
 
-            var countries = JsonSerializer.Deserialize<List<Country>>(responseString, options);
-
-            // Pass the list of countries to the view
-            ViewData["Countries"] = countries;
-
-            return View(user);
+            return JsonSerializer.Deserialize<List<Country>>(responseString, options);
         }
-        
+
         [Authorize]
         [HttpPost("/account/changepassword")]
         public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordModel model)
@@ -177,26 +187,20 @@ namespace WebApp.Controllers
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             var user = await _userService.GetAsync(userId);
             if (user == null)
-            {
                 return NotFound();
-            }
 
             if (user.Password != model.CurrentPassword)
-            {
                 return Json(new { success = false, message = "Current password is incorrect." });
-            }
 
             if (model.NewPassword == model.CurrentPassword)
-            {
                 return Json(new { success = false, message = "New password cannot be the same as the current password." });
-            }
 
             user.Password = model.NewPassword;
+            
             await _userService.PutAsync(user);
-
             return Json(new { success = true });
         }
-        
+
         [Authorize]
         [HttpPost("/account/updatesetting")]
         public async Task<IActionResult> UpdateSetting([FromBody] UpdateSettingModel model)
@@ -204,9 +208,7 @@ namespace WebApp.Controllers
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             var user = await _userService.GetAsync(userId);
             if (user == null)
-            {
                 return NotFound();
-            }
 
             switch (model.SettingName)
             {
@@ -221,10 +223,9 @@ namespace WebApp.Controllers
             }
 
             await _userService.PutAsync(user);
-
             return Ok();
         }
-        
+
         [Authorize]
         [HttpPost("/account/deleteaccount")]
         public async Task<IActionResult> DeleteAccount([FromBody] DeleteAccountModel model)
@@ -232,30 +233,15 @@ namespace WebApp.Controllers
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             var user = await _userService.GetAsync(userId);
             if (user == null)
-            {
                 return NotFound();
-            }
 
             if (user.Password != model.Password)
-            {
                 return Json(new { success = false, message = "Incorrect password." });
-            }
 
             await _userService.DeleteAsync(user.Id);
-
-            // Sign out the user
+            
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-
             return Json(new { success = true });
         }
-
-        // [HttpGet("courses")]
-        // public async Task<IActionResult> Courses()
-        // {
-        //     // Implement your courses logic here...
-        //     return View();
-        // }
-        
-        
     }
 }
