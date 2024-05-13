7a83d52b79a3d0c439936e2b2a2455ba557df320
pinkstoney <hrynivinstitutionalized@gmail.com>
2024-05-13 15:48:53 +0300

    new(about): add "about us" page

--- WebApp/Controllers/AboutController.cs
+++ WebApp/Controllers/AboutController.cs
@@ -2,16 +2,20 @@ using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 using WebApp.Models;
 using WebApp.Services;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace WebApp.Controllers
 {
     public class AboutController : Controller
     {
         private readonly MongoDBService<User> _userService;
+        private readonly MongoDBService<Course> _courseService;
 
-        public AboutController( MongoDBService<User> userService)
+        public AboutController(MongoDBService<User> userService, MongoDBService<Course> courseService)
         {
             _userService = userService;
+            _courseService = courseService;
         }
 
         private async Task<User> GetCurrentUserAsync()
@@ -19,18 +23,31 @@ namespace WebApp.Controllers
             return await _userService.GetAsync(User.FindFirstValue(ClaimTypes.NameIdentifier));
         }
 
-        private async Task<IActionResult> RenderViewAsync(string viewName)
+        private async Task<IActionResult> RenderViewAsync(string viewName, object model)
         {
             var user = await GetCurrentUserAsync();
-            return View(viewName, user);
+            return View(viewName, new { User = user, Metrics = model });
         }
 
-        public Task<IActionResult> Index() => RenderViewAsync(nameof(Index));
-        
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userService.GetAsync();
+            var courses = await _courseService.GetAsync();
+
+            var metrics = new
+            {
+                UserCount = users.Count,
+                CourseCount = courses.Count,
+                CreatorCount = users.Count(u => u.Role == "creator")
+            };
+
+            return await RenderViewAsync(nameof(Index), metrics);
+        }
+
         [Route("/about/terms")]
-        public Task<IActionResult> Terms() => RenderViewAsync(nameof(Terms));
+        public Task<IActionResult> Terms() => RenderViewAsync(nameof(Terms), null);
 
         [Route("/about/privacy")]
-        public Task<IActionResult> Privacy() => RenderViewAsync(nameof(Privacy));
+        public Task<IActionResult> Privacy() => RenderViewAsync(nameof(Privacy), null);
     }
 }
