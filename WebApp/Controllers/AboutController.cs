afbc674e15440ec995525bb6c045faa12c463368
Dmytro <dsumcow@gmail.com>
2024-05-14 11:55:31 +0300

    Revert "Merge branch 'develop' into courses"
    This reverts commit b4f199a04c4cd87e2c3cf66cd7ea6c6afa672631, reversing
    changes made to 3b75f346043ebe626c3e0928c08ac383f6cc4c6d.

--- WebApp/Controllers/AboutController.cs
+++ WebApp/Controllers/AboutController.cs
@@ -2,20 +2,16 @@ using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 using WebApp.Models;
 using WebApp.Services;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApp.Controllers
 {
     public class AboutController : Controller
     {
         private readonly MongoDBService<User> _userService;
-        private readonly MongoDBService<Course> _courseService;
 
-        public AboutController(MongoDBService<User> userService, MongoDBService<Course> courseService)
+        public AboutController( MongoDBService<User> userService)
         {
             _userService = userService;
-            _courseService = courseService;
         }
 
         private async Task<User> GetCurrentUserAsync()
@@ -23,31 +19,18 @@ namespace WebApp.Controllers
             return await _userService.GetAsync(User.FindFirstValue(ClaimTypes.NameIdentifier));
         }
 
-        private async Task<IActionResult> RenderViewAsync(string viewName, object model)
+        private async Task<IActionResult> RenderViewAsync(string viewName)
         {
             var user = await GetCurrentUserAsync();
-            return View(viewName, new { User = user, Metrics = model });
+            return View(viewName, user);
         }
 
-        public async Task<IActionResult> Index()
-        {
-            var users = await _userService.GetAsync();
-            var courses = await _courseService.GetAsync();
-
-            var metrics = new
-            {
-                UserCount = users.Count,
-                CourseCount = courses.Count,
-                CreatorCount = users.Count(u => u.Role == "creator")
-            };
-
-            return await RenderViewAsync(nameof(Index), metrics);
-        }
-
+        public Task<IActionResult> Index() => RenderViewAsync(nameof(Index));
+        
         [Route("/about/terms")]
-        public Task<IActionResult> Terms() => RenderViewAsync(nameof(Terms), null);
+        public Task<IActionResult> Terms() => RenderViewAsync(nameof(Terms));
 
         [Route("/about/privacy")]
-        public Task<IActionResult> Privacy() => RenderViewAsync(nameof(Privacy), null);
+        public Task<IActionResult> Privacy() => RenderViewAsync(nameof(Privacy));
     }
 }
